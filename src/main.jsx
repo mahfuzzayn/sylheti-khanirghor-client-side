beb7c8069be3419a6a2bf762de99303f6582732c
Mahfuz The Programmer Vetern <mahfuzzayn8@gmail.com>
2023-05-03 00:13:25 +0600

    Fixed missing codes of Routes, main.jsx and Updated project codes to latest

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes/Routes";
-
+import AuthProvider from "./context/AuthProvider";
+import "./index.css";
+import "./App.css";
+import "flowbite";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        
+        <AuthProvider>
             <RouterProvider router={router} />
-        
+        </AuthProvider>
     </React.StrictMode>
 );
--- src/routes/routes/Routes.jsx
+++ src/routes/routes/Routes.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layout/Main";
+import Home from "../../pages/Home/Home/Home";
+import Login from "../../pages/Home/Login/Login";
+import Register from "../../pages/Home/Register/Register";
+import Blog from "../../pages/Home/Blog/Blog";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <h1>Khanir Ghor</h1>
+        element: <Main></Main>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>,
+            },
+            {
+                path: "/blog",
+                element: <Blog></Blog>,
+            },
+            {
+                path: "/login",
+                element: <Login></Login>,
+            },
+            {
+                path: "/register",
+                element: <Register></Register>,
+            },
+        ],
     },
 ]);
 
